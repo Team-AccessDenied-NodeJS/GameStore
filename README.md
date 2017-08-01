7583e0ff60738ea783587f37369c9b0d53050ca8
purlantov <ppurlantov@yahoo.com>
2017-08-01 08:19:49 +0300

    users.data unit-tests, ReadMe.md, index.pug FIXES

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # GameStore
 _Teamwork project_
 
+[Website](http://access-denied.sytes.net/)
+
 [![Build Status](http://78.90.245.184:8080/buildStatus/icon?job=GameStoreApp)](http://78.90.245.184:8080/job/GameStoreApp/)
 
 ### Team Access Denied - TelerikAcademy Season 8
@@ -10,8 +12,6 @@ _Teamwork project_
 - [Daniel Mitkov](https://github.com/boda66)
 
 - [Pavel Purlantov](https://github.com/purlantov)
-
-- [Sevdalin Zhelyazkov](https://github.com/SevdalinZhelyazkov)
 
 ----------------------
 
--- gamestore/test/unit/data/users-data-tests.js
+++ gamestore/test/unit/data/users-data-tests.js
@@ -9,6 +9,211 @@ describe('UsersData', () => {
 
     let data = null;
 
+        const ModelClass = class { };
+
+    const validator = null;
+
+    let users = [];
+
+    let find = () => {
+        return {
+            toArray() {
+                return Promise.resolve(users);
+            },
+        };
+    };
+
+    const findOne = (props) => {
+        const id = props._id;
+        const item = users.find((i) => i.id === id);
+        return Promise.resolve(item || null);
+    };
+
+    describe('expect getAll()', () => {
+        beforeEach(() => {
+            sinon.stub(db, 'collection').callsFake(() => {
+                return { find };
+            });
+
+            data = new UsersData(db, ModelClass, validator);
+        });
+
+        afterEach(() => {
+            db.collection.restore();
+        });
+
+        it('to return empty array, when no users', (done) => {
+            users = [];
+            data.getAll()
+                .then((todos) => {
+                    expect(todos).to.eql(users);
+                    done();
+                }
+                );
+        }
+        );
+    });
+
+
+    describe('expect getAll()', () => {
+        beforeEach(() => {
+            sinon.stub(db, 'collection').callsFake(() => {
+                return { find };
+            });
+
+            data = new UsersData(db, ModelClass, validator);
+        });
+
+        afterEach(() => {
+            db.collection.restore();
+        });
+
+        it('to return same array as provided one', (done) => {
+            users = [1, 2, 3, 4];
+            data.getAll()
+                .then((todos) => {
+                    expect(todos).to.eql(users);
+                    done();
+                }
+                );
+        }
+        );
+    });
+
+    describe('expect findById()', () => {
+        beforeEach(() => {
+            sinon.stub(db, 'collection').callsFake(() => {
+                return { findOne };
+            });
+
+            data = new UsersData(db);
+        });
+
+        afterEach(() => {
+            db.collection.restore();
+        });
+
+        it('to return null, when no users', (done) => {
+            const { ObjectID } = require('mongodb');
+            const id = new ObjectID().toHexString();
+            data.findById(id)
+                .then((user) => {
+                    expect(user).to.be.eq(null);
+                    done();
+                });
+        });
+    });
+
+    describe('expect filterBy()', () => {
+        beforeEach(() => {
+            users = [
+                'Gosho',
+                'Pesho',
+                'Ivan',
+            ];
+
+            find = (property) => {
+                return {
+                    toArray() {
+                        return Promise.resolve(users[property]);
+                    },
+                };
+            };
+
+            sinon.stub(db, 'collection').callsFake(() => {
+                return { find };
+            });
+
+            data = new UsersData(db);
+        });
+
+        afterEach(() => {
+            db.collection.restore();
+        });
+
+        it('to filter by given title', (done) => {
+            const props = 1;
+            data.filterBy(props)
+                .then((result) => {
+                    expect(result).to.be.eq('Pesho');
+                    done();
+                }
+                );
+        });
+    });
+
+    describe('expect updateById()', () => {
+        const updateOne = (obekt) => {
+            return true;
+        };
+
+        beforeEach(() => {
+            sinon.stub(db, 'collection').callsFake(() => {
+                return { updateOne };
+            });
+
+            data = new UsersData(db);
+        });
+
+        afterEach(() => {
+            db.collection.restore();
+        });
+
+        it('to do its stuff', (done) => {
+            const obekt = {
+                _id: 5,
+            };
+
+            const result = data.updateById(obekt);
+            expect(result).to.be.eq(true);
+            done();
+        });
+    });
+
+    describe('expect isModelValid()', () => {
+        const isValid = (model) => {
+            return true;
+        };
+
+        beforeEach(() => {
+            sinon.stub(db, 'validator').callsFake(() => {
+                return { isValid };
+            });
+
+            data = new UsersData(db, ModelClass, validator);
+        });
+
+        it('to take emplty model and return false', (done) => {
+            const model = {};
+            const result = data._isModelValid(model);
+            expect(result).to.be.eq(true);
+            done();
+        });
+    });
+
+    describe('_isModelValid()', () => {
+        // const isValid = (model) => {
+        //     return true;
+        // };
+
+        const isValid = null;
+
+        beforeEach(() => {
+            sinon.stub(db, validator).returns(() => {
+                sinon.stub(validator, isValid).returns(true);
+            });
+
+            data = new UsersData(db, ModelClass, validator);
+        });
+
+        it('should take valid model and retun true', (done) => {
+            const model = { title: 'Ivan', price: 100 };
+            const result = data._isModelValid(model);
+            expect(result).to.be.eq(true);
+            done();
+        });
+    });
+
     describe('expect create()', () => {
         beforeEach(() => {
             const insert = (model) => {
